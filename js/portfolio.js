3b2c14a99eefbf8aa71d8250229f1bcb29f875e7
Азалия Вафина <azaliya.v1995@gmail.com>
2019-09-01 19:36:14 +0200

    pop-up added on card click

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,30 @@
-;let a = document.querySelector("#fly");
-let bird = document.querySelector("#bird");
-a.onclick = (e) => {
+;
+//var a = document.querySelector("#fly");
+var bird = document.querySelector("#bird");
+/*
+a.onclick = function(e) {
     e.preventDefault();
   //bird.style.backgroundColor = "blue";
   bird.classList.toggle("fly");
+}
+*/
+
+/*for overlay*/
+var overlay = document.querySelector(".overlay");
+
+var open = document.querySelector(".open-popup")
+
+open.onclick = function(e) {
+  e.preventDefault();
+    var popup = document.querySelector(".popup");
+    popup.classList.add("show");
+    overlay.style.display="block";
+  }
+
+  var close = document.querySelector(".close-popup");
+close.onclick = function(e) {
+e.preventDefault();
+	var popup = document.querySelector(".popup");
+  popup.classList.remove("show");
+  overlay.style.display="none";
 }
